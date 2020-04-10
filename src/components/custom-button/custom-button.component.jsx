383c6794db615228b56c4ae269b38a2e3ed2e17b
John Wells <molekewl2017@gmail.com>
2020-04-09 18:21:38 -0700

    add cart utils to group items in cart reducer

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 import './custom-button.styles.scss';
 
+// on the form it needs type='submit' & value='Submit form' children replaces
+// 'value' so it can be removed from the submit button.
+// Passing ...otherProps will handle type='submit' in the form
 const CustomButton = ({ children, isGoogleSignIn, inverted, ...otherProps }) => (
   <button
     className={`${inverted ? 'inverted' : ''} ${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
