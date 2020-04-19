4e062c09c02c790ebdfa75e60283ed0c24b88cf2
John Wells <molekewl2017@gmail.com>
2020-04-19 13:14:39 -0700

    move data into a reducer, created better flow of data passing through the components

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,12 +1,13 @@
 import { createSelector } from 'reselect';
 
+/* used for line 23. Converted the shop_data to an object
 const COLLECTION_ID_MAP = {
   hats: 1,
   sneakers: 2,
   jackets: 3,
   womens: 4,
   mens: 5
-};
+}; */
 
 const selectShop = (state) => state.shop;
 
@@ -15,10 +16,18 @@ export const selectCollections = createSelector(
   (shop) => shop.collections
 );
 
+// CollectionsOverview Component was accessing the data before as an
+// array. Convert so it can access the new data as an object
+export const selectCollectionsForPreview = createSelector(
+  [selectCollections],
+  collections => Object.keys(collections).map(key => collections[key])
+);
+
 export const selectCollection = (collectionUrlParam) =>
-  createSelector([selectCollections], 
-    (collections) =>
-    collections.find(
-      (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-    )
+  createSelector(
+    [selectCollections], 
+      (collections) =>
+      // collections.find(
+      // (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
+      collections[collectionUrlParam]
   );
