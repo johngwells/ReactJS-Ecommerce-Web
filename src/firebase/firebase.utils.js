7ea389d6bdc90839c2d61c4bf7951174669ffa9b
John Wells <molekewl2017@gmail.com>
2020-04-26 13:56:13 -0700

    code in firebase utils to add the data from shop.data.js to firebase automatically without having to add the data manually

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -41,6 +41,19 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   return userRef;
 };
 
+// A way to add shop.data.js into the firebase store. then remove the code once its added
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+}
+
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
