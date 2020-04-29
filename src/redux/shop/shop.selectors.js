9ce689fa63cd455bd380a2183a65e518da86c3fa
John Wells <molekewl2017@gmail.com>
2020-04-28 23:36:59 -0700

    change code in components to access the data from the backend and not the shopdata.js file | created a loading spinner

--- src/redux/shop/shop.selectors.js
+++ src/redux/shop/shop.selectors.js
@@ -1,14 +1,5 @@
 import { createSelector } from 'reselect';
 
-/* used for line 23. Converted the shop_data to an object
-const COLLECTION_ID_MAP = {
-  hats: 1,
-  sneakers: 2,
-  jackets: 3,
-  womens: 4,
-  mens: 5
-}; */
-
 const selectShop = (state) => state.shop;
 
 export const selectCollections = createSelector(
@@ -20,14 +11,11 @@ export const selectCollections = createSelector(
 // array. Convert so it can access the new data as an object
 export const selectCollectionsForPreview = createSelector(
   [selectCollections],
-  collections => Object.keys(collections).map(key => collections[key])
+  collections => collections ? Object.keys(collections).map(key => collections[key]) : []
 );
 
 export const selectCollection = (collectionUrlParam) =>
   createSelector(
     [selectCollections], 
-      (collections) =>
-      // collections.find(
-      // (collection) => collection.id === COLLECTION_ID_MAP[collectionUrlParam]
-      collections[collectionUrlParam]
+      (collections) => collections ? collections[collectionUrlParam] : null
   );
