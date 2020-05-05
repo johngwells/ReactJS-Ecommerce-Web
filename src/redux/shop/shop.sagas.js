890bcff049717f7699b7832883a36412be8236db
John Wells <molekewl2017@gmail.com>
2020-05-04 22:57:07 -0700

    add redux-saga

---
+++ src/redux/shop/shop.sagas.js
@@ -0,0 +1,14 @@
+import { takeEvery } from 'redux-saga/effects';
+
+import ShopActionTypes from './shop.types';
+
+export function* fetchCollectionAsync() {
+  yield console.log('I am fired');
+}
+
+export function* fetchCollectionsStart() {
+  yield takeEvery(
+    ShopActionTypes.FETCH_COLLECTIONS_START,
+    fetchCollectionAsync
+  );
+}
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,19 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
-import thunk from 'redux-thunk';
+import createSagaMiddleware from 'redux-saga'
+
+import { fetchCollectionsStart } from './shop/shop.sagas';
 
 import rootReducer from './root-reducer';
 
-const middlewares = [logger, thunk];
+const sagaMiddleware = createSagaMiddleware();
+
+const middlewares = [logger, sagaMiddleware];
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
+
+sagaMiddleware.run(fetchCollectionsStart);
 
 export const persistor = persistStore(store);
 
